8aa7339fdbc3b7b8db156ac6161658039dc5eab1
Shreyash Parashar <ipl04shreyashp@iimrohtak.ac.in>
2026-08-12 22:15:29 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Root from "./Shop.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>
+);
